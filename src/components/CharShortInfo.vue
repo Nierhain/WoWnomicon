4034697365dd4953dbfc89df1410a0c8e91f5da0
Robin Meischke <robin@nierhain.de>
2020-01-29 13:52:01 +0100

    initial file tree

---
+++ src/components/CharShortInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CharShortInfo",
+  props: {}
+};
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 200px;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <CharShortInfo />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CharShortInfo from "@/components/CharShortInfo.vue";
+export default {
+  name: "home",
+  components: {
+    CharShortInfo
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #5d5d5d;
+}
+</style>
